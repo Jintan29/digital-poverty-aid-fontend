446e4a80baab1fcf0c136c37da9a10820b1215c3
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2024-12-18 17:39:18 +0700

    add store , Login , Reg , fix navbar , add config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,71 @@
-import { useState } from "react";
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 
 import NewNav from "./components/NewNav";
 import { About } from "./pages/About";
 import { Form } from "./pages/Form";
+import Register from "./pages/Authentication/Register";
+import { Login } from "./pages/Authentication/Login";
+import Swal from "sweetalert2";
+import axios from "axios";
+import config from "./config";
+import { useDispatch } from "react-redux";
+import { login } from "./store/userSlice";
 
 
-// import { NewNav } from "./components/NewNav";
 
 
 function App() {
-  
+  const idToken = localStorage.getItem('token')
+  const dispatch = useDispatch()
 
+  useEffect(()=>{
+    if(idToken){
+      handleCurrentUser(idToken)
+    }
+  },[idToken])
+
+
+  const handleCurrentUser = async(idToken)=>{
+    try {
+      await axios.get(config.api_path+'/user/currrentUser',config.headers())
+      .then(res=>{
+        console.log(res);
+        dispatch(
+          login({
+            name: res.data.username,
+            role: res.data.role,
+            status: res.data.status,
+            token:idToken
+          })
+        )
+      }).catch(err=>{throw err})
+    }catch(e){
+      Swal.fire({
+        icon:'error',
+        text:e,
+        title:'error'
+      })
+    }
+  }
+  
   return (
     <>
-      {/* ส่วนนี้จะแสดงผลทุกหน้า */}
-      <NewNav />
-
       <BrowserRouter>
-      <div className="App">
-        {/* ดึงหน้า Page มาใช้งาน */}
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/form' element={<Form/>}/>
-        </Routes>
-
-      </div>
-      
+        <NewNav />
+        <div className="App">
+          {/* ดึงหน้า Page มาใช้งาน */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </div>
       </BrowserRouter>
-
-
     </>
   );
 }
