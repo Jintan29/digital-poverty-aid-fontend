771c22a2605446bae3afb91816db196fdce9614c
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2024-12-19 13:11:57 +0700

    add proxy and submit fect api in fristpage

--- src/components/Form/FristPage.jsx
+++ src/components/Form/FristPage.jsx
@@ -319,21 +319,34 @@ export const FristPage = ({
     }
   }, [formData.Household.district]);
 
-  //ยิงAPI
-  const handleSubmitForm = (e)=>{
-    e.preventDefault();
-    setMainFormData((prevData) => ({
-      ...prevData,
-      ...formData, //นำไปต่อท้ายค่าใน State เก่า
-    }));
-  }
-
-
+  // const handleFetch = async()=>{
+  //   try{
+  //     await axios.get('/sradss/api/01poor.php?API-TOKEN=MzBiZDdhY2EyODY1YWNiZmU0Nzc0OW&&province_id=65&&yearget=all&mode=1')
+  //     .then(res=>{
+  //       console.log(res);
+        
+  //     })
+  //   }catch(e){
+  //     alert(e)
+  //   }
+  // }
 
   return (
     <div>
       <div>
         <form onSubmit={handleSubmit}>
+        {/* <button
+              type="button"
+              onClick={e=>handleFetch()}
+              class="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              ทดสอบยิง API
+              <Icon
+                icon="material-symbols:arrow-right-rounded"
+                width="25"
+                height="25"
+              />
+            </button> */}
           <div class="mb-6 mx-10 mt-10">
             <label
               for="house_code"
