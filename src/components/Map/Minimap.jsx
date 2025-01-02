cd2c016ec284644b78a0a5df8832e743e83ce0e1
Sky <146667716+PhonsawanS@users.noreply.github.com>
2025-01-02 20:10:54 +0700

    add map function 2/1/2568 20:10

--- src/components/Map/Minimap.jsx
+++ src/components/Map/Minimap.jsx
@@ -3,13 +3,36 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Map2 from "./Map2"
-import Map from "./Map";
+import Map1 from "./Map1";
 
 
 
 function Minimap() {
 
     const [householdCount, setHouseholdCount] = useState(0);
+    const [memberHouseholdCount, setMemberHouseholdCount] = useState(0);
+
+    // useEffect(() => {
+    //     const fetchCounts = async () => {
+    //       try {
+    //         // ใช้ Promise.all เพื่อเรียก API ทั้งสองพร้อมกัน
+    //         const [householdResponse, memberHouseholdResponse] = await Promise.all([
+    //           axios.get("http://localhost:8080/api/house-hold/count"),
+    //           axios.get("http://localhost:8080/api/member-household/count")
+    //         ]);
+            
+    //         // ตั้งค่าจำนวนสมาชิกในครัวเรือน
+    //         setHouseholdCount(householdResponse.data.count);
+    //         // ตั้งค่าจำนวนสมาชิกในบ้าน
+    //         setMemberHouseholdCount(memberHouseholdResponse.data.data);
+    
+    //       } catch (error) {
+    //         console.error("Error fetching household count:", error);
+    //       }
+    //     };
+    
+    //     fetchCounts();
+    //   }, []); // ใช้ [] เพื่อให้โหลดแค่ครั้งเดียว
 
     useEffect(() => {
         const fetchHouseholdCount = async () => {
@@ -23,6 +46,21 @@ function Minimap() {
 
         fetchHouseholdCount();
     }, []);
+
+
+    useEffect(() => {
+      const fetchMemberHouseholdCount = async () => {
+        try {
+          const response = await axios.get("http://localhost:8080/api/member-household/count");
+          setMemberHouseholdCount(response.data.data); // ใช้ response.data.data เพื่อเข้าถึงค่าที่ส่งกลับจาก backend
+        } catch (error) {
+          console.error("Error fetching household count:", error);
+        }
+      };
+      fetchMemberHouseholdCount();
+    }, []); // ใช้ [] เพื่อให้โหลดแค่ครั้งเดียว
+
+
 
 
     // const [showMap, setShowMap] = useState(true);
@@ -49,59 +87,83 @@ function Minimap() {
     };
 
     return (
-        <div className="flex justify-between">
-            <div className="w-1/2 p-24 mt-[20px]">
+        <div className="flex justify-between mt-4 items-stretch overflow-hidden">
+            {/* ซ้าย */}
+            <div className="w-1/2 p-6 h-screen flex flex-col justify-start pt-20">
                 {/* ส่วนแรก */}
-                <div className="p-2">
-                    <h3 className="text-center ml-14 text-[24px] font-bold text-shadow-custom">
+                <div className="p-4">
+                    <h3 className="text-center text-[24px] font-bold text-shadow-custom">
                         จำนวนครัวเรือนฯ
                     </h3>
-                    <p className=" text-center ml-14 text-[18px]">
+                    <p className="text-center text-[18px]">
                         จำนวนครัวเรือนฯ ที่ได้จากการสำรวจ
                     </p>
-                    <p className="text-center ml-16 text-[22px] font-bold text-red-500">
-                    {householdCount.toLocaleString()} <span className="text-black font-normal text-shadow-custom">ครัวเรือน</span>
+                    <p className="text-center text-[22px] font-bold text-red-500">
+                        {householdCount.toLocaleString()} <span className="text-black font-normal text-shadow-custom">ครัวเรือน</span>
                     </p>
                 </div>
                 {/* ส่วนสอง */}
-                <div className="p-2 mt-6">
-                    <h3 className="text-center ml-16 text-[24px] font-bold text-shadow-custom">
+                <div className="p-4 mt-6">
+                    <h3 className="text-center text-[24px] font-bold text-shadow-custom">
                         จำนวนสมาชิกฯ
                     </h3>
-                    <p className="text-center ml-14 text-[18px]">
+                    <p className="text-center text-[18px]">
                         จำนวนสมาชิกที่ได้จากการสำรวจ
                     </p>
-                    <p className="text-center ml-20 text-[22px] font-bold text-red-500">
-                        58,560  <span className="text-black font-normal text-shadow-custom">คน</span>
+                    <p className="text-center text-[22px] font-bold text-red-500">
+                        {memberHouseholdCount.toLocaleString()} <span className="text-black font-normal text-shadow-custom">คน</span>
                     </p>
                 </div>
             </div>
 
-            {/* ซ้าย */}
-            <div className="w-1/2 p-4 mt-6">
-                <div className="flex justify-center items-center">
-                    <div
-                        className="bg-blue-500 text-white px-2 py-1 rounded-l-full font-medium text-sm cursor-pointer"
-                        onClick={() => handleClick(true)} // กดแสดง Map
-                    >
-                        จำนวนครัวเรือนยากจน
+            {/* ขวา */}
+            <div className="w-1/2 h-screen flex flex-col">
+                {/* บน */}
+                <div className="h-1/6 p-4">
+                    <div className="flex justify-start items-center">
+                        <div
+                            className="bg-blue-500 text-white px-2 py-1 rounded-l-full font-medium text-sm cursor-pointer"
+                            onClick={() => handleClick(true)} // กดแสดง Map
+                        >
+                            จำนวนครัวเรือนยากจน
+                        </div>
+                        <div
+                            className="bg-yellow-400 text-white px-2 py-1 rounded-r-full font-medium text-sm cursor-pointer"
+                            onClick={() => handleClick(false)} // กดแสดง Map2
+                        >
+                            จำนวนสมาชิกครัวเรือนยากจน
+                        </div>
                     </div>
-                    <div
-                        className="bg-yellow-400 text-white px-2 py-1 rounded-r-full font-medium text-sm cursor-pointer"
-                        onClick={() => handleClick(false)} // กดแสดง Map2
-                    >
-                        จำนวนสมาชิกครัวเรือนยากจน
-                    </div>
+                    {/* แสดงข้อความในสีที่แตกต่างกันตามแผนที่ */}
+                    {showMap ? (
+                        <p className="text-[22px] font-bold text-blue-500 mt-2">
+                            {householdCount.toLocaleString()}{" "}
+                            <span className="text-black font-normal text-shadow-custom">ครัวเรือน</span>
+                        </p>
+                    ) : (
+                        <p className="text-[22px] font-bold text-yellow-500 mt-2">
+                            {memberHouseholdCount.toLocaleString()}{" "}
+                            <span className="text-black font-normal text-shadow-custom">คน</span>
+                        </p>
+                    )}
+
                 </div>
 
-                {/* แสดงแผนที่ตามการเลือก */}
-                <div>
-                    {showMap ? <Map /> : <Map2 />}
+                {/* ล่าง */}
+                <div className="h-5/6 p-4">
+                    {/* แสดงแผนที่ตามการเลือก */}
+                    <div>
+                        {showMap ?
+                            <Map1 /> : <Map2 />}
+                    </div>
                 </div>
             </div>
+        </div>
 
-        </div >
+
     );
 }
 
 export default Minimap;
+
+
