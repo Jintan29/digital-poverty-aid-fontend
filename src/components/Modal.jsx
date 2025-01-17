b13ef437b750c9e46cbafe879b3546a838ec778d
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2025-01-17 17:48:09 +0700

    add modal components

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import { Modal as FlowbiteModal } from "flowbite-react";
+import { Icon } from "@iconify/react";
 
-const Modal = ({ title, show, onClose, children, size = "md" }) => {
-    return (
-      <FlowbiteModal
-        show={show}         // แสดงหรือซ่อน modal
-        onClose={onClose}   // ฟังก์ชันเรียกปิด modal
-        dismissible={true}  // ทำให้กด backdrop หรือปุ่ม close แล้วปิดได้
-        size={size}
-      >
-        <FlowbiteModal.Header>
-          {title}
-        </FlowbiteModal.Header>
-        <FlowbiteModal.Body>
-          {children}
-        </FlowbiteModal.Body>
-        <FlowbiteModal.Footer>
-          {/* ถ้าต้องการปุ่มเพิ่ม เช่น save หรือ close ก็ใส่ในนี้ได้ */}
-          <button
-            className="btn btn-primary"
-            onClick={onClose}
-          >
-            ปิด
-          </button>
-        </FlowbiteModal.Footer>
-      </FlowbiteModal>
-    );
-  };
+const Modal = ({ title, show, onClose, children, size = "md", icon }) => {
+  return (
+    <FlowbiteModal
+      show={show} // แสดงหรือซ่อน modal
+      onClose={onClose} // ฟังก์ชันเรียกปิด modal
+      dismissible={true} // ทำให้กด backdrop หรือปุ่ม close แล้วปิดได้
+      size={size}
+    >
+      <FlowbiteModal.Header className="bg-gray-600 [&>h3]:!text-white">
+        <div className="flex items-center">
+        {icon &&
+        <Icon
+        icon={icon}
+        width={25}
+        height={25}
+        className="mr-4"
+      />
+        }
+        {title}
+        </div>
+        
+      </FlowbiteModal.Header>
+      <FlowbiteModal.Body>{children}</FlowbiteModal.Body>
+      <FlowbiteModal.Footer className="flex justify-end">
+        <button
+          type="button"
+          onClick={onClose}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          บันทึก
+        </button>
+      </FlowbiteModal.Footer>
+    </FlowbiteModal>
+  );
+};
 
-export default Modal
+export default Modal;
